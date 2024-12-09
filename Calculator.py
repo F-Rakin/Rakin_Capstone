243a8afa36b5a75a6bf42e91a4f78accb4ec1991
F-Rakin <rakinfaizanahmad@gmail.com>
2024-12-08 21:10:31 -0800

    Cleanup

--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,31 @@
+"""Faizan A. Rakin"""
+
 print("Welcome to Calc")
+
+# Call functions every time a button is pressed on my GUI
+
+def add(): 
+    """
+    This function will be used to add numbers inputted
+    """ 
+    pass
+
+def subtract():
+    """
+    This function will be used to subtract one number from another
+    """
+    pass
+
+def multiply():
+    """"
+    This function will be used to multiply numbers
+    """
+    pass
+
+def divide():
+    """
+    This function will be used to divide numbers
+    """
+    pass
+
 
--- ListsPractice.py
+++
@@ -1,3 +0,0 @@
-# This doc file will have practice of Lists
-# Practice will inlcude random runs and tests to understand functions
-# Notes of syntax and concepts included
